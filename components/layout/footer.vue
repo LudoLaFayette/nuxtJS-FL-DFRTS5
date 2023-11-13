e8cce350f8347761ffa726b2ee307173ee46d151
LudoLaFayette <ludovicfaye89@gmail.com>
2023-11-13 23:21:06 +0100

    footer renommage de class scss

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -4,42 +4,42 @@ import myTitle from '@/components/elements/myTitle.vue'
 import myText from '@/components/elements/myText.vue'
 </script>
 <template>
-  <footer class="footer">
-    <div class="footer__column">
-      <img class="footer__logo" src="../../public/logo-footer.jpg" alt="Restaurant Logo" />
+  <footer class="c-footer">
+    <div class="c-footer__column">
+      <img class="c-footer__column--logo" src="../../public/logo-footer.jpg" alt="Restaurant Logo" />
       <myText
         size="p2"
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo libero viverra dapibus odio sit malesuada in quis. Arcu tristique elementum viverra integer id."
       />
-      <div class="footer__social_icons">
+      <div class="c-footer__column--social_icons">
         <myIcon name="facebook" background="backgroundTT" size="small" />
         <myIcon name="twitter" background="backgroundTT" size="small" />
         <myIcon name="linkedin" background="backgroundTT" size="small" />
         <myIcon name="instagram" background="backgroundTT" size="small" />
       </div>
     </div>
-    <div class="footer__column">
-      <myTitle class="footer__title" content="Opening Restaurant" type="h4" />
-      <myText class="footer__text" size="p1" content="Monday-Friday: 9AM - 10PM" />
-      <myText class="footer__text" size="p1" content="Thursdayt: 09AM - 11PM" />
-      <myText class="footer__text" size="p1" content="Friday: 09AM - 8PM" />
+    <div class="c-footer__column">
+      <myTitle class="c-footer__column--title" content="Opening Restaurant" type="h4" />
+      <myText class="c-footer__column--text" size="p1" content="Monday-Friday: 9AM - 10PM" />
+      <myText class="c-footer__column--text" size="p1" content="Thursdayt: 09AM - 11PM" />
+      <myText class="c-footer__column--text" size="p1" content="Friday: 09AM - 8PM" />
     </div>
-    <div class="footer__column">
-      <myTitle class="footer__title" content="User Links" type="h4" />
-      <ul class="footer__list-link">
-        <li class="footer__text"><a href="#">About Us</a></li>
-        <li class="footer__text"><a href="#">Contact Us</a></li>
-        <li class="footer__text"><a href="#">Order Delivery</a></li>
-        <li class="footer__text"><a href="#">Payment & Tax</a></li>
-        <li class="footer__text"><a href="#">Terms of Services</a></li>
+    <div class="c-footer__column">
+      <myTitle class="c-footer__column--title" content="User Links" type="h4" />
+      <ul class="c-footer__column--list-link">
+        <li class="c-footer__column--text"><a href="#">About Us</a></li>
+        <li class="c-footer__column--text"><a href="#">Contact Us</a></li>
+        <li class="c-footer__column--text"><a href="#">Order Delivery</a></li>
+        <li class="c-footer__column--text"><a href="#">Payment & Tax</a></li>
+        <li class="c-footer__column--text"><a href="#">Terms of Services</a></li>
       </ul>
     </div>
-    <div class="footer__column">
-      <myTitle class="footer__title" content="Contact Us" type="h4" />
-      <myText class="footer__text" size="p1" content="1234 Country Club Ave" />
-      <myText class="footer__text" size="p1" content="NC 123456, London, UK" />
-      <myText class="footer__text" size="p1" content="+0123 456 7891" />
-      <div class="footer__input">
+    <div class="c-footer__column">
+      <myTitle class="c-footer__column--title" content="Contact Us" type="h4" />
+      <myText class="c-footer__column--text" size="p1" content="1234 Country Club Ave" />
+      <myText class="c-footer__column--text" size="p1" content="NC 123456, London, UK" />
+      <myText class="c-footer__column--text" size="p1" content="+0123 456 7891" />
+      <div class="c-footer__column--input">
         <input type="email" id="email" placeholder="Enter your email...." />
         <myIcon class="" name="chevronRight" size="small" background="backgroundT" />
       </div>
@@ -47,7 +47,7 @@ import myText from '@/components/elements/myText.vue'
   </footer>
 </template>
   <style lang="scss" scoped>
-.footer {
+.c-footer {
   background: $light-grey;
   color: $grey;
   display: grid;
@@ -57,32 +57,32 @@ import myText from '@/components/elements/myText.vue'
 
   &__column {
     padding: 1rem;
-  }
-  &__logo {
+
+    &--logo {
     padding-bottom: rem(15);
   }
-  &__social_icons {
+  &--social_icons {
     display: flex;
     margin: rem(15) rem(5);
     gap: rem(15);
   }
-  &__title {
+  &--title {
     font-weight: 700;
     color: $black;
     padding: rem(45) rem(0);
   }
-  &__text {
+  &--text {
     padding: rem(5) rem(0);
     font-weight: 400;
   }
-  &__list-link {
+  &--list-link {
     list-style: none;
     & li a {
       text-decoration: none;
       color: $grey;
     }
   }
-  &__input {
+  &--input {
     background-color: $white;
     border-radius: rem(40);
     box-shadow: 0px rem(1) rem(4) rgba(0, 0, 0, 0.25);
@@ -99,6 +99,8 @@ import myText from '@/components/elements/myText.vue'
       border: none;
       outline: none;
     }
+  }
+  
   }
 }
 </style>
